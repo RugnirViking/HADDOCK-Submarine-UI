97150f2460060dd4b2caf98f8248b56f2cfe0228
Samuel Howell <rugnirtheviking@gmail.com>
2020-05-06 19:06:48 +0200

    add status panel

--- UIPackage/src/simple_view/hello.py
+++ UIPackage/src/simple_view/hello.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
+import cmd
+import subprocess
 import rospy
 import math
 import pygame, sys
 import pygame.camera
+import pygame.locals 
 import os 
 import datetime
 from geometry_msgs.msg import Vector3
 from std_msgs.msg import Int16
+from std_msgs.msg import String
 
 white = (255, 255, 255) 
 darkblue = (10, 0, 60) 
@@ -18,7 +22,7 @@ navBallSizeN = 360
 
 camSize = (320,240)
 
-screenXSize = 1200
+screenXSize = 1350
 screenYSize = 800
 
 roll = 0
@@ -27,6 +31,70 @@ heading = 360
 time = 0
 depth = 10
 
+pitch_input_val = 0
+roll_input_val = 0
+heading_input_val = 0
+altitude_input_val = 0
+
+cmd_response_text = ""
+
+
+mode1_enabled = True
+mode2_enabled = False
+mode3_enabled = False
+control_enabled = True
+brush_enabled=False
+emergencyStop_enabled=False
+emergencyStop_reallyEnabled=False
+logging_enabled=False
+verbose_enabled=False
+
+currentLogfile = None
+
+class Shell_Class(cmd.Cmd):
+    """Simple command processor example."""
+    
+    # Disable rawinput module use
+    use_rawinput = False
+    
+    last_output = ''
+
+    def do_shell(self, line):
+		global cmd_response_text
+        #"Run a shell command"
+		print "running shell command:", line
+		try:
+			cmd = subprocess.Popen(line,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+			output, error = cmd.communicate()
+			#print cmd_response_text
+			if(error):
+				print(error)
+			else:
+				self.last_output = output
+				cmd_response_text = output
+
+		except OSError as e: 
+			print("inside exception", e)
+			self.last_output = str(e)
+			cmd_response_text = str(e)
+		except AttributeError as e: 
+			print("inside exception", e)
+			self.last_output = str(e)
+			cmd_response_text = str(e)
+		except:
+			cmd_response_text = sys.exc_info()[0]
+		#print output
+    
+    def do_echo(self, line):
+        "Print the input, replacing '$out' with the output of the last shell command"
+        # Obviously not robust
+        print line.replace('$out', self.last_output)
+    
+    def do_greet(self, line):
+        print "hello,", line
+    
+    def do_EOF(self, line):
+        return True
 
 def IMU_message_recieved(data):
 	global roll
@@ -40,6 +108,15 @@ def Depth_message_recieved(data):
 	global depth
 	depth = data.data
 	rospy.logdebug("Depth Message recieved. Value: "+str(data.data))
+
+def log_message_recieved(data):
+	global logging_enabled
+	global verbose_enabled
+
+	if logging_enabled and verbose_enabled:
+		currentLogfile.write(data.data+"\n")
+	elif logging_enabled:
+		currentLogfile.write(data.data.split("||")[0]+"\n")
 
 def rot_center(image, angle):
 	#"""rotate an image while keeping its center and size"""
@@ -76,12 +153,39 @@ def say(name):
 	global heading
 	global time
 	global depth
+	global cmd_response_text
+	global pitch_input_val
+	global roll_input_val
+	global heading_input_val
+	global altitude_input_val
+	global mode1_enabled 
+	global mode2_enabled 
+	global mode3_enabled 
+	global control_enabled 
+	global brush_enabled
+	global emergencyStop_enabled
+	global logging_enabled
+	global verbose_enabled
+	global emergencyStop_reallyEnabled
+	global currentLogfile
+
+	console_active=False
 
 
 	# basic setup
 	rospy.init_node('python_sub_controller', anonymous=True) # node is called 'python_sub_controller'
 	rospy.Subscriber("imu_values_topic", Vector3, IMU_message_recieved)
 	rospy.Subscriber("depth_topic", Int16, Depth_message_recieved)
+	rospy.Subscriber("python_submarine_logger", String, log_message_recieved)
+	pitch_pub = rospy.Publisher('pitch_control_input', Int16, queue_size=10)
+	roll_pub = rospy.Publisher('roll_control_input', Int16, queue_size=10)
+	heading_pub = rospy.Publisher('heading_control_input', Int16, queue_size=10)
+	altitude_pub = rospy.Publisher('altitude_control_input', Int16, queue_size=10)
+
+	mode_pub = rospy.Publisher('control_mode', Int16, queue_size=10)
+	brush_pub = rospy.Publisher('brush_toggle', Int16, queue_size=10)
+	emergencyStopPub = rospy.Publisher('r', Int16, queue_size=10)
+
 	rospy.loginfo("Loading Control Screen...")
 	pygame.init()
 	pygame.camera.init()
@@ -91,8 +195,10 @@ def say(name):
 
 	pygame.display.set_caption('Submarine Prototype')
 	title_font = pygame.font.Font('freesansbold.ttf', 32) 
-	info_font = pygame.font.Font('freesansbold.ttf', 12) 
-	camera_overlay_font = pygame.font.Font('freesansbold.ttf', 12) 
+	info_font = pygame.font.SysFont('Sawasdee', 12,True) 
+	console_font = pygame.font.SysFont('Lucida Console', 14,True) 
+	panel_font = pygame.font.SysFont('Sawasdee', 25,True) 
+	camera_overlay_font = pygame.font.SysFont('Lucida Console', 12,True) 
 
 	carImg = pygame.image.load('resources/aaulogo.png') # AAU Logo
 	#navImg = pygame.image.load('resources/navball.png') # Navball
@@ -121,6 +227,7 @@ def say(name):
 	navImg.convert()
 	navImg = pygame.transform.scale(navImg, (1400,1400))
 
+	console_text = ""
 	# Define update rate, which includes max fps. Having both ros 
 	# messaging and graphics in the same thread is probably bad theoretically, but 
 	# hopefully we can get away with it by not being too taxing on the gpu
@@ -176,6 +283,8 @@ def say(name):
 		depth_textRect.topleft = (x+328, 264) 
 		windowSurfaceObj.blit(depth_text, depth_textRect) 
 
+
+		## DRAW CAMERAS
 		if cam.query_image():
 			snapshot = cam.get_image(snapshot)
 
@@ -195,7 +304,7 @@ def say(name):
 		fCamtextRect.topleft = (x+4, 454) 
 		windowSurfaceObj.blit(rCamText, fCamtextRect) 
 
-		
+		## DRAW NAVBALL
 		navBallPos=(600-navBallSizeN/2,508-navBallSizeN/2)
 		NavBallImg(navImg,windowSurfaceObj,600,510,roll,pitch,heading)
 
@@ -237,20 +346,223 @@ def say(name):
 			rollSpiritBubbleX=rollSpiritMinX
 		else: 
 			rollSpiritBubbleX = rollSpiritMinX+(0.5+roll*0.1)*rollSpiritX
-		pygame.draw.rect(windowSurfaceObj, (0,140,0), (rollSpiritBubbleX, navCentreY-37,boxX*0.2-spiritLevel_padding*2, 18-spiritLevel_padding), 0)
+		pygame.draw.rect(windowSurfaceObj, (0,140,0), (rollSpiritBubbleX, navCentreY-37,boxX*0.2-spiritLevel_padding*2+1, 18-spiritLevel_padding), 0)
 
 		pygame.draw.rect(windowSurfaceObj, white, (navCentreX+boxX/2-boxX/5/2,navCentreY+boxY/2,boxX/5,5), 2)
 		pygame.draw.line(windowSurfaceObj, white, (navCentreX+boxX/2-boxX/5, navCentreY+boxY/2), (navCentreX+boxX/2+boxX/5, navCentreY+boxY/2), 3)
 
+		## DRAW TERMINAL
+		terminal_bg_color=(0,0,0)
+		if console_active:
+			terminal_bg_color = (100,100,100)
+		else:
+			terminal_bg_color = (50,50,50)
+		
+
+		text = info_font.render("INFO CONSOLE", True, (0,0,0), white) 
+		textRect = text.get_rect()  
+		textRect.topleft = (screenXSize*0.65+8,screenYSize*0.32+4) 
+		windowSurfaceObj.blit(text, textRect)
+
+		
+
+		# setup terminal
+		input_box = pygame.Rect(screenXSize*0.65,screenYSize*0.83,400,24)
+
+		pygame.draw.rect(windowSurfaceObj, terminal_bg_color, input_box, 0)
+		pygame.draw.rect(windowSurfaceObj, (0,0,0), (screenXSize*0.65,screenYSize*0.63,400,screenYSize*0.2), 0)
+		pygame.draw.rect(windowSurfaceObj, (0,0,0), (screenXSize*0.65,screenYSize*0.35,400,screenYSize*0.2), 0)
+
+		text = console_font.render(console_text, True, white, terminal_bg_color) 
+		textRect = text.get_rect()  
+		textRect.topleft = (screenXSize*0.65+8,screenYSize*0.83+4) 
+		windowSurfaceObj.blit(text, textRect) 
+
+
+		pygame.draw.rect(windowSurfaceObj, (160,160,160), (screenXSize*0.15,screenYSize*0.10,screenXSize*0.80-16,96), 0)
+
+		text = info_font.render("COMMAND CONSOLE", True, (0,0,0), white) 
+		textRect = text.get_rect()  
+		textRect.topleft = (screenXSize*0.65+8,screenYSize*0.6+4) 
+		windowSurfaceObj.blit(text, textRect)
+		
+
+		cmd_response_lines = cmd_response_text.split("\n")
+		i=0
+		for line in cmd_response_lines:
+			if len(line)>0:
+				text = console_font.render(line, True, white, (0,0,0)) 
+				textRect = text.get_rect()  
+				textRect.topleft = (screenXSize*0.65+8,screenYSize*0.63+4+i*14) 
+				windowSurfaceObj.blit(text, textRect) 
+			i+=1
+
+		## DRAWING STATUS PANEL
+
+		for x in range(0,5):
+			for y in range(0,2):
+				button_enabled=False
+				button_color = white
+				current_text = "EGG"
+				if x==0 and y==0:
+					if mode1_enabled:
+						button_enabled = True
+					current_text = " MODE1 "
+					button_color = (255,255,0)
+				if x==0 and y==1:
+					if mode2_enabled:
+						button_enabled = True
+					current_text = " MODE2 "
+					button_color = (0,255,0)
+				if x==1 and y==0:
+					if mode3_enabled:
+						button_enabled = True
+					current_text = "MODE3"
+					button_color = (0,255,255)
+				if x==1 and y==1:
+					if control_enabled:
+						button_enabled = True
+					current_text = "CONTROL"
+					button_color = (255,170,0)
+				if x==2 and y==0:
+					if brush_enabled:
+						button_enabled = True
+					current_text = "BRUSH"
+					button_color = (170,0,255)
+				if x==2 and y==1:
+					current_text = "ESTOP"
+					if emergencyStop_enabled:
+						button_enabled = True
+						current_text = "ESTOP ARMED"
+					button_color = (140,140,140)
+					if emergencyStop_reallyEnabled:
+						current_text = "ESTOP ACTIVE!!"
+						button_color = (255,0,0)
+				if x==3 and y==0:
+					if logging_enabled:
+						button_enabled = True
+					current_text = "LOGGING"
+					button_color = (0,255,170)
+				if x==3 and y==1:
+					if verbose_enabled:
+						button_enabled = True
+					current_text = "VERBOSE"
+					button_color = (0,255,220)
+				if button_enabled:
+					text = panel_font.render(current_text, True, button_color, (130,130,130)) 
+					pygame.draw.rect(windowSurfaceObj, (130,130,130), (screenXSize*0.15+4+(screenXSize*0.16-4)*x,screenYSize*0.10+4+46*y,screenXSize*0.16-8,50-8), 0)
+				else:
+					text = panel_font.render(current_text, True, (140,140,140), (110,110,110)) 
+					pygame.draw.rect(windowSurfaceObj, (110,110,110), (screenXSize*0.15+4+(screenXSize*0.16-4)*x,screenYSize*0.10+4+46*y,screenXSize*0.16-8,50-8), 0)
+
+				textRect = text.get_rect()  
+				textRect.center = (screenXSize*0.15+4+(screenXSize*0.16-4)*x+(screenXSize*0.16-8)/2,screenYSize*0.10+4+46*y+(50-8)/2) 
+				windowSurfaceObj.blit(text, textRect) 
 		###
 		### GRAPHICS GOES ABOVE HERE /\   /\   /\
 		###
 
 		pygame.display.update()
+
+
+		pitch_pub.publish(pitch_input_val)
+		roll_pub.publish(roll_input_val)
+		heading_pub.publish(heading_input_val)
+		altitude_pub.publish(altitude_input_val)
+		
+		pitch_input_val = 0
+		roll_input_val = 0
+		heading_input_val = 0
+		altitude_input_val = 0
+
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_w]:
+			pitch_input_val=1
+		elif keys[pygame.K_s]:
+			pitch_input_val=-1
+		elif keys[pygame.K_a]:
+			roll_input_val=1
+		elif keys[pygame.K_d]:
+			roll_input_val=-1
+		elif keys[pygame.K_q]:
+			heading_input_val=1
+		elif keys[pygame.K_e]:
+			heading_input_val=-1
+		elif keys[pygame.K_SPACE]:
+			altitude_input_val=1
+		elif keys[pygame.K_LSHIFT]:
+			altitude_input_val=-1
 		# if the user presses the cross button on the window, close it.
 		for event in pygame.event.get() : 
 			if event.type == pygame.QUIT : 
 				cam.stop()
+				currentLogfile.close()
 				pygame.quit() 
 				quit() 
+			if event.type == pygame.MOUSEBUTTONDOWN:
+                # If the user clicked on the input_box rect.
+				if input_box.collidepoint(event.pos):
+					# Toggle the active variable.
+					console_active = not console_active
+					control_enabled = not control_enabled
+				else:
+					console_active = False
+			if event.type == pygame.KEYDOWN:
+				if console_active:
+					if event.key == pygame.K_RETURN:
+						print("Operator entered command: "+console_text)
+						Shell_Class().onecmd(console_text)
+						console_text = ''
+					elif event.key == pygame.K_BACKSPACE:
+						console_text = console_text[:-1]
+					else:
+						console_text += event.unicode
+				else:
+					if event.key == pygame.K_1:
+						mode1_enabled = True
+						mode2_enabled = False
+						mode3_enabled = False
+						brush_enabled = False
+						mode_pub.publish(1)
+						brush_pub.publish(0)
+					elif event.key == pygame.K_2:
+						mode1_enabled = False
+						mode2_enabled = True
+						mode3_enabled = False
+						brush_enabled = False
+						brush_pub.publish(0)
+					elif event.key == pygame.K_3:
+						mode1_enabled = False
+						mode2_enabled = False
+						mode3_enabled = True
+						mode_pub.publish(3)
+					elif event.key == pygame.K_HASH:
+						if emergencyStop_enabled:
+							emergencyStop_reallyEnabled = True
+						emergencyStop_enabled = True
+					elif event.key == pygame.K_RIGHTBRACKET:
+						if emergencyStop_enabled and not emergencyStop_reallyEnabled:
+							emergencyStop_enabled=False
+							emergencyStopPub.publish(1)
+					elif event.key == pygame.K_TAB:
+						if mode3_enabled and not brush_enabled:
+							brush_enabled = True
+							brush_pub.publish(1)
+						else:
+							brush_enabled=False
+							brush_pub.publish(0)
+
+					elif event.key == pygame.K_l:
+						
+						logging_enabled = not logging_enabled
+						verbose_enabled = False
+						if logging_enabled:
+							currentLogfile = open("logs_"+str(rospy.get_time()),"w")
+						else:
+							currentLogfile.close()
+					elif event.key == pygame.K_SEMICOLON:
+						if logging_enabled:
+							verbose_enabled = not verbose_enabled
+				### Control Schema
+				
 		rate.sleep()
